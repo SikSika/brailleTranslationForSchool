a49f2aa3154ce40e3cf29f2c8e80ddc7a44df6eb
SikSika <109840362+SikSika@users.noreply.github.com>
2024-01-07 18:10:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,57 @@ function checkScroll() {
 }
 
 document.addEventListener('scroll', checkScroll);
+
+// Send Gmail
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const senderName = form.name.value;
+  const senderEmail = form.email.value;
+  const bookTitle = form.bookTitle.value;
+  const authorName = form.author.value;
+  const phoneNumber = form.phone.value;
+  const message = form.message.value;
+  const shippingAddress = form.address.value;
+
+  // Disable the submit button
+  form.querySelector("button[type='submit']").disabled = true;
+
+  const formData = new FormData();
+  formData.append("name", senderName);
+  formData.append("email", senderEmail);
+  formData.append("author", authorName);
+  formData.append("phone", phoneNumber);
+  formData.append("message", message);
+  formData.append("address", shippingAddress);
+
+  fetch("https://script.google.com/macros/s/AKfycbyhudbl2TWK04pLvuTdJ4Xlu96Nl07oElXEPLql0smOjqs-Eg88zTHjbCaEi2EajsyllQ/exec", {
+    method: "POST",
+    body: formData,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        alert("Your request has been submitted successfully!");
+        form.reset();
+      } else {
+        alert("Oops! Something went wrong. Please try again later.");
+      }
+    })
+    .catch(function (error) {
+      alert("Oops! Something went wrong. Please try again later.");
+    })
+    .finally(function () {
+      // Re-enable the submit button after 1 second
+      setTimeout(function () {
+        form.querySelector("button[type='submit']").disabled = false;
+      }, 1000);
+    });
+});
+
+
+
+
+
+
+
